fa690d3bffbdb7eaa325c65b68fefe920d1a43d6
genywind <evgeny@rhomobile.com>
2011-04-07 19:01:54 +0400

    add on_sync_update_error and on_sync_delete_error
    Conflicts:
    
    	spec/phone_spec/app/spec/syncengine_spec.rb

--- spec/phone_spec/app/spec/syncengine_spec.rb
+++ spec/phone_spec/app/spec/syncengine_spec.rb
@@ -376,6 +376,7 @@ describe "SyncEngine_test" do
     res['server_errors']['create-error'].should_not be_nil
     res['server_errors']['create-error'][item.object].should_not be_nil    
     res['server_errors']['create-error'][item.object]['message'].should == "error create"
+    res['server_errors']['create-error'][item.object]['attributes']['name'].should == "wrongname"
 
     records2 = getTestDB().select_from_table('changed_values','*', 'update_type' => 'create')
     records2.length.should == ($spec_settings[:schema_model] ? 7 : 3)
@@ -425,6 +426,7 @@ describe "SyncEngine_test" do
     res['server_errors']['create-error'].should_not be_nil
     res['server_errors']['create-error'][item.object].should_not be_nil    
     res['server_errors']['create-error'][item.object]['message'].should == "error create"
+    res['server_errors']['create-error'][item.object]['attributes']['name'].should == "wrongname"
 
     records2 = getTestDB().select_from_table('changed_values','*', 'update_type' => 'create')
     records2.length.should == ($spec_settings[:schema_model] ? 7 : 3)
@@ -477,6 +479,7 @@ describe "SyncEngine_test" do
     res['server_errors']['create-error'].should_not be_nil
     res['server_errors']['create-error'][item.object].should_not be_nil    
     res['server_errors']['create-error'][item.object]['message'].should == "error create"
+    res['server_errors']['create-error'][item.object]['attributes']['name'].should == "wrongname"
 
     records2 = getTestDB().select_from_table('changed_values','*', 'update_type' => 'create')
     records2.length.should == ($spec_settings[:schema_model] ? 7 : 3)
@@ -493,7 +496,8 @@ describe "SyncEngine_test" do
     records2[0]['attrib'].should_not == 'object'        
     records2[0]['sent'].should == 0
 
-    getProduct.on_sync_create_error( res['server_errors']['create-error'].keys(), :recreate)
+    #getProduct.on_sync_create_error( res['server_errors']['create-error'].keys(), :recreate)
+    SyncEngine.on_sync_create_error( getProduct_str(), res['server_errors']['create-error'], :recreate)
     
     records3 = getTestDB().select_from_table('changed_values','*', 'object' => item.object)
     records3.length.should == 0
@@ -517,7 +521,24 @@ describe "SyncEngine_test" do
     res['server_errors']['update-error'].should_not be_nil
     res['server_errors']['update-error']['broken_object_id'].should_not be_nil    
     res['server_errors']['update-error']['broken_object_id']['message'].should == "error update"
+    res['server_errors']['update-error']['broken_object_id']['attributes']['name'].should == "wrongname"    
+
+    records = getTestDB().select_from_table('changed_values','*', 'update_type' => 'update')
+    records.length.should == 0
     
+    getProduct.on_sync_update_error( res['server_errors']['update-error'], :retry)
+    
+    records1 = getTestDB().select_from_table('changed_values','*', 'update_type' => 'update')
+    records1.length.should == 2
+    records1[0]["object"].should == "broken_object_id"
+    records1[0]["attrib"].should == "name"
+    records1[0]["value"].should == "wrongname"
+    records1[0]["update_type"].should == "update"
+
+    records1[1]["object"].should == "broken_object_id"
+    records1[1]["attrib"].should == "an_attribute"
+    records1[1]["value"].should == "error update"
+    records1[1]["update_type"].should == "update"
   end
 
   it "should process delete-error" do    
@@ -531,6 +552,24 @@ describe "SyncEngine_test" do
     res['server_errors']['delete-error'].should_not be_nil
     res['server_errors']['delete-error']['broken_object_id'].should_not be_nil    
     res['server_errors']['delete-error']['broken_object_id']['message'].should == "Error delete record"
+    res['server_errors']['delete-error']['broken_object_id']['attributes']['name'].should == "wrongname"
+    
+    records = getTestDB().select_from_table('changed_values','*', 'update_type' => 'delete')
+    records.length.should == 0
+    
+    getProduct.on_sync_delete_error( res['server_errors']['delete-error'], :retry)
+    
+    records1 = getTestDB().select_from_table('changed_values','*', 'update_type' => 'delete')
+    records1.length.should == 2
+    records1[0]["object"].should == "broken_object_id"
+    records1[0]["attrib"].should == "name"
+    records1[0]["value"].should == "wrongname"
+    records1[0]["update_type"].should == "delete"
+
+    records1[1]["object"].should == "broken_object_id"
+    records1[1]["attrib"].should == "an_attribute"
+    records1[1]["value"].should == "error delete"
+    records1[1]["update_type"].should == "delete"
     
   end
 
@@ -544,7 +583,6 @@ describe "SyncEngine_test" do
     res['server_errors'].should_not be_nil
     res['server_errors']['query-error'].should_not be_nil
     res['server_errors']['query-error']['message'].should == "Error during query"
-    
   end
 
   it "should process search-error" do    
