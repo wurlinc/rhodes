b7a8594e464908de40ba9b8de43fe2427e00adc5
David Martinez <david@hackerdude.com>
2011-09-08 10:02:21 -0700

    Also listen in on activity results.

--- platform/android/Rhodes/src/com/rhomobile/rhodes/RhodesActivityListener.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/RhodesActivityListener.java
@@ -11,5 +11,7 @@ public interface RhodesActivityListener {
 	void onNewIntent(RhodesActivity activity, Intent intent);
 	
 	void onRhodesActivityStartup(RhodesActivity activity);
-	
+
+	void onActivityResult(int requestCode, int resultCode, Intent data);
+
 }
