c51178ea09220f93104bd650558f44171c0881d3
David Martinez <david@hackerdude.com>
2012-02-14 16:09:00 -0800

    SyncEngine is a module when defined, not a class. So stubs should be the same.

--- lib/test/rho_stubs.rb
+++ lib/test/rho_stubs.rb
@@ -14,7 +14,7 @@
    end
  end
 
- class SyncEngine
+ module SyncEngine
    @@mutex = ::Mutex.new()
    def self.lock_sync_mutex
      @@mutex.lock
@@ -22,6 +22,18 @@
    def self.unlock_sync_mutex
      @@mutex.unlock
    end
+	 def self.clean_objectnotify(*args)
+	 end
+	 def self.set_pollinterval(value)
+	 end
+	 def self.start_sync
+	 end
+	 def self.stop_sync
+	 end
+	 def self.update_blob_attribs(*args)
+	 end
+	 def self.dosearch(*args)
+	 end
  end
 
  class RhoConf
